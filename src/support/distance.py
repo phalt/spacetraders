4958d473d924b958362be24e082f9f2c3efc2dbb
Paul Hallett <paul.hallett@sharesies.co.nz>
2023-06-19 15:34:56 +1200

    Add maths for calculating distance

---
+++ src/support/distance.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from numpy import array, linalg
+
+
+def euclidean_distance(point_a: List[int], point_b: List[int]) -> float:
+    """
+    Calculate the distance between points in 3D space.
+    Points must be an array of X,Y values.
+    """
+
+    loc_1 = array(point_a)
+    loc_2 = array(point_b)
+
+    return linalg.norm(loc_1 - loc_2)
